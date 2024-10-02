817ea43e1a439497b3d83cb6bfa90dd3adff534f
Leo <git@gkbrk.com>
2024-10-02 20:40:37 +0300

    Refactor Logging Macros with Generalized Macro
    This commit introduces a generalized `log_level!` macro to centralize
    and streamline logging functionality across different log levels. By
    leveraging `log_level!`, specific log macros (`trace!`, `debug!`,
    `info!`, `warn!`, and `error!`) are simplified, reducing code
    duplication and enhancing maintainability. Additionally, the generic
    `log!` macro is retained to forward logs to the `debug!` level, ensuring
    backward compatibility.

--- src/log.rs
+++ src/log.rs
@@ -4,64 +4,57 @@ pub(super) const INFO_ENABLED: bool = true;
 pub(super) const WARN_ENABLED: bool = true;
 pub(super) const ERROR_ENABLED: bool = true;
 
+// Generalized logging macro
 #[macro_export]
-macro_rules! log {
-  ($($arg:tt)*) => {
-    $crate::debug!($($arg)*);
-  };
+macro_rules! log_level {
+    ($level_enabled:expr, $level_str:expr, $($arg:tt)*) => {
+        if $level_enabled {
+            let now = chrono::Local::now();
+            let timestamp = now.format("%H:%M:%S");
+            eprintln!("[{}] [{}] {}", timestamp, $level_str, format_args!($($arg)*));
+        }
+    };
 }
 
+// Specific log level macros using the generalized macro
 #[macro_export]
 macro_rules! trace {
-  ($($arg:tt)*) => {
-    if $crate::log::TRACE_ENABLED {
-      let now = chrono::Local::now();
-      let timestamp = now.format("%H:%M:%S");
-      eprintln!("[{}] [TRC] {}", timestamp, format_args!($($arg)*));
-    }
-  };
+    ($($arg:tt)*) => {
+        $crate::log_level!($crate::log::TRACE_ENABLED, "TRC", $($arg)*);
+    };
 }
 
 #[macro_export]
 macro_rules! debug {
-  ($($arg:tt)*) => {
-    if $crate::log::DEBUG_ENABLED {
-      let now = chrono::Local::now();
-      let timestamp = now.format("%H:%M:%S");
-      eprintln!("[{}] [DBG] {}", timestamp, format_args!($($arg)*));
-    }
-  };
+    ($($arg:tt)*) => {
+        $crate::log_level!($crate::log::DEBUG_ENABLED, "DBG", $($arg)*);
+    };
 }
 
 #[macro_export]
 macro_rules! info {
-  ($($arg:tt)*) => {
-    if $crate::log::INFO_ENABLED {
-      let now = chrono::Local::now();
-      let timestamp = now.format("%H:%M:%S");
-      eprintln!("[{}] [INF] {}", timestamp, format_args!($($arg)*));
-    }
-  };
-}
-
-#[macro_export]
-macro_rules! error {
-  ($($arg:tt)*) => {
-    if $crate::log::ERROR_ENABLED {
-      let now = chrono::Local::now();
-      let timestamp = now.format("%H:%M:%S");
-      eprintln!("[{}] [ERR] {}", timestamp, format_args!($($arg)*));
-    }
-  };
+    ($($arg:tt)*) => {
+        $crate::log_level!($crate::log::INFO_ENABLED, "INF", $($arg)*);
+    };
 }
 
 #[macro_export]
 macro_rules! warn {
-  ($($arg:tt)*) => {
-    if $crate::log::WARN_ENABLED {
-      let now = chrono::Local::now();
-      let timestamp = now.format("%H:%M:%S");
-      eprintln!("[{}] [WRN] {}", timestamp, format_args!($($arg)*));
-    }
-  };
+    ($($arg:tt)*) => {
+        $crate::log_level!($crate::log::WARN_ENABLED, "WRN", $($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {
+        $crate::log_level!($crate::log::ERROR_ENABLED, "ERR", $($arg)*);
+    };
+}
+
+#[macro_export]
+macro_rules! log {
+    ($($arg:tt)*) => {
+        $crate::debug!($($arg)*);
+    };
 }
