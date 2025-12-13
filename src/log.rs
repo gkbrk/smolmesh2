8778f6beffa72d25155e4342f58d223141869bf9
Leo <git@gkbrk.com>
2025-12-13 16:45:21 +0300

    Small cleanup of log macros
    I removed the semicolons so I can use these as expressions (like in
    match statements).

--- src/log.rs
+++ src/log.rs
@@ -20,41 +20,41 @@ macro_rules! log_level {
 #[macro_export]
 macro_rules! trace {
     ($($arg:tt)*) => {
-        $crate::log_level!($crate::log::TRACE_ENABLED, "TRC", $($arg)*);
+        $crate::log_level!($crate::log::TRACE_ENABLED, "TRC", $($arg)*)
     };
 }
 
 #[macro_export]
 macro_rules! debug {
     ($($arg:tt)*) => {
-        $crate::log_level!($crate::log::DEBUG_ENABLED, "DBG", $($arg)*);
+        $crate::log_level!($crate::log::DEBUG_ENABLED, "DBG", $($arg)*)
     };
 }
 
 #[macro_export]
 macro_rules! info {
     ($($arg:tt)*) => {
-        $crate::log_level!($crate::log::INFO_ENABLED, "INF", $($arg)*);
+        $crate::log_level!($crate::log::INFO_ENABLED, "INF", $($arg)*)
     };
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($arg:tt)*) => {
-        $crate::log_level!($crate::log::WARN_ENABLED, "WRN", $($arg)*);
+        $crate::log_level!($crate::log::WARN_ENABLED, "WRN", $($arg)*)
     };
 }
 
 #[macro_export]
 macro_rules! error {
     ($($arg:tt)*) => {
-        $crate::log_level!($crate::log::ERROR_ENABLED, "ERR", $($arg)*);
+        $crate::log_level!($crate::log::ERROR_ENABLED, "ERR", $($arg)*)
     };
 }
 
 #[macro_export]
 macro_rules! log {
     ($($arg:tt)*) => {
-        $crate::debug!($($arg)*);
+        $crate::debug!($($arg)*)
     };
 }
